98bba12c67249830becb66a145c7ebef3e0e6472
GIULIANO MARCO <giuliano.lember@gmail.com>
2022-09-13 23:19:45 +0300

    (3) 1 done 2 done 3 done 4 done

--- Kontrolltoo Mang/Tegelane.cs
+++ Kontrolltoo Mang/Tegelane.cs
@@ -8,6 +8,27 @@ namespace Kontrolltoo_Mang
 {
     internal class Tegelane : Uksus
     {
+        private string nimi;
+        List<Ese> eseList = new List<Ese>();
+
+        public Tegelane(string nimi)
+        {
+            this.nimi = nimi;
+        }
+
+        public int liseEse(int ese) { return ese; }
+
+        public int punktide_arv()
+        {
+            int damage=0;
+            foreach (Ese ese in eseList)
+            {
+                damage += ese.PunktideArv();
+            }
+            return damage;
+        }
+
+
         public string Info()
         {
             throw new NotImplementedException();
