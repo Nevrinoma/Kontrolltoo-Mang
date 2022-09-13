ffb268841b03ba46123914e762cc05eae668cf39
GIULIANO MARCO <giuliano.lember@gmail.com>
2022-09-13 23:00:07 +0300

    Add project files.

---
+++ Kontrolltoo Mang/Ese.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontrolltoo_Mang
+{
+    internal class Ese : Uksus
+    {
+        private string nimetus;
+        private int punktideArv;
+
+        public string Info()
+        {
+            return nimetus;
+        }
+
+        public int PunktideArv()
+        {
+            return punktideArv;
+        }
+        public Ese(string nimetus, int punktideArv)
+        {
+            this.nimetus = nimetus;
+            this.punktideArv = punktideArv;
+        }
+
+    }
+
+
+}
---
+++ Kontrolltoo Mang/Uksus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontrolltoo_Mang
+{
+    internal interface Uksus
+    {
+        int PunktideArv();
+        string Info();
+    }
+}
